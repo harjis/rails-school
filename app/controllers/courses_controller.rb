0aec9f0c605e71ec151882079ef36ecdc4745b0a
Joonas Harjukallio <harjukallio@gmail.com>
2019-04-25 11:36:47 +0300

    Beautify

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,21 +4,21 @@ class CoursesController < ApplicationController
   end
 
   def index_includes
-    @courses = Course.all.includes(:teachers).includes(:students)
+    @courses = Course.all.includes(:teachers, :students)
 
     render 'courses/index'
   end
 
   # This fetches everything with one query
   def index_eager_load
-    @courses = Course.all.eager_load(:teachers).eager_load(:students)
+    @courses = Course.all.eager_load(:teachers, :students)
 
     render 'courses/index'
   end
 
   # This gives wrong result
   def index_joins
-    @courses = Course.all.joins(:teachers).joins(:students)
+    @courses = Course.all.joins(:teachers, :students)
 
     render 'courses/index'
   end
