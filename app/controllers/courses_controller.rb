0ea1bd6eab63a8b2c8c1b77cdef08410cefaa3a4
Joonas Harjukallio <harjukallio@gmail.com>
2019-04-25 11:33:09 +0300

    Add a few more

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,6 +9,13 @@ class CoursesController < ApplicationController
     render 'courses/index'
   end
 
+  # This fetches everything with one query
+  def index_eager_load
+    @courses = Course.all.eager_load(:teachers).eager_load(:students)
+
+    render 'courses/index'
+  end
+
   # This gives wrong result
   def index_joins
     @courses = Course.all.joins(:teachers).joins(:students)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'courses/', to: 'courses#index'
   get 'courses/includes', to: 'courses#index_includes'
+  get 'courses/eager', to: 'courses#index_eager_load'
   get 'courses/joins', to: 'courses#index_joins'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
